4307dbf7fd18f643d83122101c4a8bde348bf9e5
Excellence Atode <iamanexcellentchild@gmail.com>
2026-06-28 05:36:20 +0100

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 from app.models.user import User
-from app.models.exam import Exam, Question, Result, Answer, Topic
+from app.models.exam import Exam, Question, Result, Answer, Topic,TournamentEntry
 
 __all__ = ['User', 'Exam', 'Question', 'Result', 'Answer', 'Topic', 'TournamentEntry']
